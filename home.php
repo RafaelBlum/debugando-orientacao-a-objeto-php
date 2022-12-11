e07df2d37401b1d81b41324501d0cfb51e00fa63
Rafael Blum <rafaelblum_digital@hotmail.com>
2022-12-11 11:12:21 -0300

    Add subtitle home page

--- home.php
+++ home.php
@@ -8,6 +8,8 @@ echo "<div class='box__title'>
      </div>
      <div class='box__shadow'></div>";
 
+echo "<hr><pre>SubTitle</pre>";
+
 echo "<div class='msg__success'><i class='ri-wechat-2-line'></i>Text...</div>";
 echo "<div class='msg__warning'><i class='ri-wechat-2-line'></i>Text...</div>";
 echo "<div class='msg__alert'><i class='ri-wechat-2-line'></i>Text...</div>";
